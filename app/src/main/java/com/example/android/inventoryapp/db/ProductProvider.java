ca1e8609a644329b85ac230c6171d9de8bf6798e
Michael Dijamco <mdd233@cornell.edu>
2016-06-29 18:02:32 -0400

    fix: Finish query()

--- app/src/main/java/com/example/android/inventoryapp/db/ProductProvider.java
+++ app/src/main/java/com/example/android/inventoryapp/db/ProductProvider.java
@@ -4,6 +4,7 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 
@@ -59,15 +60,17 @@ public class ProductProvider extends ContentProvider{
                         ,null
                         ,sortOrder
                 );
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
-
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
     }
 
 
-    @Nullable
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
     }
 
     @Override
