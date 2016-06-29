1f4fde08406f3b1c1a291fd09271162659822390
Michael Dijamco <mdd233@cornell.edu>
2016-06-29 17:39:13 -0400

    feat: Implement product DB schema / contract

--- app/src/main/java/com/example/android/inventoryapp/db/ProductContract.java
+++ app/src/main/java/com/example/android/inventoryapp/db/ProductContract.java
@@ -3,11 +3,27 @@ package com.example.android.inventoryapp.db;
 import android.provider.BaseColumns;
 
 /**
- * Created by mdd23 on 6/29/2016.
+ * Defines table and column names for the product database
  */
 public class ProductContract {
 
     public static final class ProductEntry implements BaseColumns {
 
+        public static final String TABLE_NAME = "product";
+
+        // Name of the product
+        public static final String COLUMN_PRODUCT = "product";
+
+        // Price of the product (xx.xx) where x are numbers
+        public static final String COLUMN_PRICE = "price";
+
+        // Quantity of the product (int)
+        public static final String COLUMN_QTY = "quantity";
+
+        // Name of the supplier of the product
+        public static final String COLUMN_SUPPLIER_NAME = "supplier_name";
+
+        // Email address of the supplier
+        public static final String COLUMN_SUPPLIER_EMAIL = "supplier_email";
     }
 }
