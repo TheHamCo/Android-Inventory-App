92bb08861c0fcf1fe04111cf96ed221637165359
Michael Dijamco <mdd233@cornell.edu>
2016-06-29 18:49:27 -0400

    feat: Layout basic product card

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.android.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView productList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        productList = (ListView) findViewById(R.id.product_list);
+
+//        SimpleCursorAdapter
+
     }
 }
