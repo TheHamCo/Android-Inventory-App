da95191330e59fa7f04125c627067584ca0ca595
Michael Dijamco <mdd233@cornell.edu>
2016-06-29 17:49:11 -0400

    feat: Add product URI

--- app/src/main/java/com/example/android/inventoryapp/db/ProductContract.java
+++ app/src/main/java/com/example/android/inventoryapp/db/ProductContract.java
@@ -1,5 +1,7 @@
 package com.example.android.inventoryapp.db;
 
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,7 +9,14 @@ import android.provider.BaseColumns;
  */
 public class ProductContract {
 
+    public static final String CONTENT_AUTHORITY = "com.example.android.inventoryapp.app";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_PRODUCT = "product";
+
     public static final class ProductEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PRODUCT).build();
 
         public static final String TABLE_NAME = "product";
 
@@ -25,5 +34,9 @@ public class ProductContract {
 
         // Email address of the supplier
         public static final String COLUMN_SUPPLIER_EMAIL = "supplier_email";
+
+        public static Uri buildLocationuri(long id){
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 }
