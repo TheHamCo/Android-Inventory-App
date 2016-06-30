0c6ddca4caa57e5c5c8c684872417dcf34bce3a1
Michael Dijamco <mdd233@cornell.edu>
2016-06-29 20:17:00 -0400

    docs: Comment increment qty

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -63,15 +63,18 @@ public class MainActivity extends AppCompatActivity {
         productList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Get the current qty
                 Cursor prodCursor = (Cursor)parent.getItemAtPosition(position);
                 int currQty = prodCursor.getInt(prodCursor.getColumnIndex(ProductEntry.COLUMN_QTY));
+                //URI of this entry
                 Uri productIdUri = ProductEntry.buildLocationuri(id);
 
                 Log.d("product id uri", productIdUri.toString());
 
+                //Update the qty
                 ContentValues values = new ContentValues();
                 values.put(ProductEntry.COLUMN_QTY, ++currQty);
-
+                
                 getContentResolver().update(productIdUri,values,null,null);
             }
         });
