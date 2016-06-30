f3a9b56fd9b076b7dd9ec7197b79d5d0f5ebc2c5
Michael Dijamco <mdd233@cornell.edu>
2016-06-30 14:05:39 -0400

    feat: Add price validation method

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -29,6 +29,7 @@ import com.example.android.inventoryapp.db.ProductContract.ProductEntry;
 
 import java.util.Currency;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
@@ -226,6 +227,53 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {}
+
+    private boolean priceIsValid(String priceString){
+
+        // Source: http://docs.oracle.com/javase/6/docs/api/java/lang/Double.html#valueOf%28java.lang.String%29
+        // Given by: http://stackoverflow.com/a/3543749/5302182
+
+        final String Digits     = "(\\p{Digit}+)";
+        final String HexDigits  = "(\\p{XDigit}+)";
+        // an exponent is 'e' or 'E' followed by an optionally
+        // signed decimal integer.
+        final String Exp        = "[eE][+-]?"+Digits;
+        final String fpRegex    =
+                ("[\\x00-\\x20]*"+  // Optional leading "whitespace"
+                        "[+-]?(" + // Optional sign character
+                        "NaN|" +           // "NaN" string
+                        "Infinity|" +      // "Infinity" string
+
+                        // A decimal floating-point string representing a finite positive
+                        // number without a leading sign has at most five basic pieces:
+                        // Digits . Digits ExponentPart FloatTypeSuffix
+                        //
+                        // Since this method allows integer-only strings as input
+                        // in addition to strings of floating-point literals, the
+                        // two sub-patterns below are simplifications of the grammar
+                        // productions from the Java Language Specification, 2nd
+                        // edition, section 3.10.2.
+
+                        // Digits ._opt Digits_opt ExponentPart_opt FloatTypeSuffix_opt
+                        "((("+Digits+"(\\.)?("+Digits+"?)("+Exp+")?)|"+
+
+                        // . Digits ExponentPart_opt FloatTypeSuffix_opt
+                        "(\\.("+Digits+")("+Exp+")?)|"+
+
+                        // Hexadecimal strings
+                        "((" +
+                        // 0[xX] HexDigits ._opt BinaryExponent FloatTypeSuffix_opt
+                        "(0[xX]" + HexDigits + "(\\.)?)|" +
+
+                        // 0[xX] HexDigits_opt . HexDigits BinaryExponent FloatTypeSuffix_opt
+                        "(0[xX]" + HexDigits + "?(\\.)" + HexDigits + ")" +
+
+                        ")[pP][+-]?" + Digits + "))" +
+                        "[fFdD]?))" +
+                        "[\\x00-\\x20]*");// Optional trailing "whitespace"
+
+        return Pattern.matches(fpRegex, priceString);
+    }
 
     private AddProductValidation ValidateAddProduct(String productName, String price, String qty, String supplierName, String supplierEmail){
         boolean isValid = true;
