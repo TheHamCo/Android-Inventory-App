6696d8b750d6409ac6e1a9521676debabfd99bb2
Michael Dijamco <mdd233@cornell.edu>
2017-06-07 12:19:37 -0400

    feat: Change SampleData from nested array to List of Hashmaps

--- app/src/main/java/com/example/android/inventoryapp/sampledata/SampleData.java
+++ app/src/main/java/com/example/android/inventoryapp/sampledata/SampleData.java
@@ -1,27 +1,30 @@
 package com.example.android.inventoryapp.sampledata;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by mdd23 on 6/5/2017.
  */
 public class SampleData implements SampleDataContract{
 
-    String[][] sampleData;
+    private List<Map<String , String>>  sampleData;
 
-    public SampleData(String[][] newSampleData) {
+    public SampleData(List<Map<String , String>> newSampleData) {
         sampleData = newSampleData;
     }
 
     public SampleData() {
-        sampleData = new String[1][6];
+
     }
 
     @Override
-    public String[][] getSampleData() {
+    public List<Map<String , String>> getSampleData() {
         return sampleData;
     }
 
     @Override
-    public void setSampleData(String[][] newSampleData) {
+    public void setSampleData(List<Map<String , String>> newSampleData) {
         sampleData = newSampleData;
     }
 }
--- app/src/main/java/com/example/android/inventoryapp/sampledata/SampleDataContract.java
+++ app/src/main/java/com/example/android/inventoryapp/sampledata/SampleDataContract.java
@@ -1,9 +1,12 @@
 package com.example.android.inventoryapp.sampledata;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by mdd23 on 6/5/2017.
  */
 public interface SampleDataContract {
-    public String[][] getSampleData();
-    public void setSampleData(String[][] newSampleData);
+    public List<Map<String , String>> getSampleData();
+    public void setSampleData(List<Map<String , String>> newSampleData);
 }
