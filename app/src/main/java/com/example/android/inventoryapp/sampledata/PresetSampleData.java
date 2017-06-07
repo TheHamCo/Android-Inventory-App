b2105dc43ae22e66d6fb42fadd824820f9c244ca
Michael Dijamco <mdd233@cornell.edu>
2017-06-07 13:08:13 -0400

    refactor: Move repeating code in PresetSampleData to private method

--- app/src/main/java/com/example/android/inventoryapp/sampledata/PresetSampleData.java
+++ app/src/main/java/com/example/android/inventoryapp/sampledata/PresetSampleData.java
@@ -1,10 +1,15 @@
 package com.example.android.inventoryapp.sampledata;
 
-import com.example.android.inventoryapp.db.ProductContract.ProductEntry;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import static com.example.android.inventoryapp.db.ProductContract.ProductEntry.COLUMN_IMAGE_URL;
+import static com.example.android.inventoryapp.db.ProductContract.ProductEntry.COLUMN_PRICE;
+import static com.example.android.inventoryapp.db.ProductContract.ProductEntry.COLUMN_PRODUCT;
+import static com.example.android.inventoryapp.db.ProductContract.ProductEntry.COLUMN_QTY;
+import static com.example.android.inventoryapp.db.ProductContract.ProductEntry.COLUMN_SUPPLIER_EMAIL;
+import static com.example.android.inventoryapp.db.ProductContract.ProductEntry.COLUMN_SUPPLIER_NAME;
 
 /**
  * Created by mdd23 on 6/7/2017.
@@ -16,61 +21,88 @@ public class PresetSampleData {
     public static SampleData getPresetData() {
         List<HashMap<String, String>> presetDataList = new ArrayList<>();
 
-        HashMap<String, String> item = new HashMap<>();
-        item.put(ProductEntry.COLUMN_PRODUCT, "Nougat");
-        item.put(ProductEntry.COLUMN_PRICE, "7.00");
-        item.put(ProductEntry.COLUMN_QTY, "7");
-        item.put(ProductEntry.COLUMN_SUPPLIER_NAME, "Android");
-        item.put(ProductEntry.COLUMN_SUPPLIER_EMAIL, "nutella@android.com");
-        item.put(ProductEntry.COLUMN_IMAGE_URL, "http://cdn03.androidauthority.net/wp-content/uploads/2016/03/android-n-preview-logo.jpg");
-        presetDataList.add(item);
+        presetDataList.add(createDataHashMap(
+                  "Nougat"
+                , "7.00"
+                , "7"
+                , "Android"
+                , "nutella@android.com"
+                , "http://cdn03.androidauthority.net/wp-content/uploads/2016/03/android-n-preview-logo.jpg"
+        ));
 
-        item = new HashMap<>();
-        item.put(ProductEntry.COLUMN_PRODUCT, "Nutella");
-        item.put(ProductEntry.COLUMN_PRICE, "10.00");
-        item.put(ProductEntry.COLUMN_QTY, "10");
-        item.put(ProductEntry.COLUMN_SUPPLIER_NAME, "Wishful Thinking");
-        item.put(ProductEntry.COLUMN_SUPPLIER_EMAIL, "nutella@android.com");
-        item.put(ProductEntry.COLUMN_IMAGE_URL, "http://www.nutella.com/nutella-gl-theme/images/custom/Nutella.png");
-        presetDataList.add(item);
+        presetDataList.add(createDataHashMap(
+                  "Ice Cream"
+                , "$2.00"
+                , "25"
+                , "Ben and Jerry's"
+                , "ice@cream.com"
+                , "http://i.imgur.com/F4k3seo.jpg"
+        ));
 
-        item = new HashMap<>();
-        item.put(ProductEntry.COLUMN_PRODUCT, "Tea");
-        item.put(ProductEntry.COLUMN_PRICE, "0.99");
-        item.put(ProductEntry.COLUMN_QTY, "3");
-        item.put(ProductEntry.COLUMN_SUPPLIER_NAME, "Dutch East India Company");
-        item.put(ProductEntry.COLUMN_SUPPLIER_EMAIL, "1602@deic.com");
-        item.put(ProductEntry.COLUMN_IMAGE_URL, "http://www.asiawelcome.com/Images/Spices/VOC_Holland_SpicesFleet_mw.jpg");
-        presetDataList.add(item);
+        presetDataList.add(createDataHashMap(
+                  "Nutella"
+                , "10.00"
+                , "10"
+                , "Wishful Thinking"
+                , "nutella@android.com"
+                , "http://www.nutella.com/nutella-gl-theme/images/custom/Nutella.png"
+        ));
 
-        item = new HashMap<>();
-        item.put(ProductEntry.COLUMN_PRODUCT, "Espresso");
-        item.put(ProductEntry.COLUMN_PRICE, "3.00");
-        item.put(ProductEntry.COLUMN_QTY, "3");
-        item.put(ProductEntry.COLUMN_SUPPLIER_NAME, "Starbucks");
-        item.put(ProductEntry.COLUMN_SUPPLIER_EMAIL, "starbucks@starbucks.com");
-        item.put(ProductEntry.COLUMN_IMAGE_URL, "http://i.imgur.com/x48dEvw.jpg");
-        presetDataList.add(item);
+        presetDataList.add(createDataHashMap(
+                  "Tea"
+                , "0.99"
+                , "3"
+                , "Dutch East India Company"
+                , "1602@deic.com"
+                , "http://www.asiawelcome.com/Images/Spices/VOC_Holland_SpicesFleet_mw.jpg"
+        ));
 
-        item = new HashMap<>();
-        item.put(ProductEntry.COLUMN_PRODUCT, "Latte");
-        item.put(ProductEntry.COLUMN_PRICE, "4.00");
-        item.put(ProductEntry.COLUMN_QTY, "3");
-        item.put(ProductEntry.COLUMN_SUPPLIER_NAME, "Starbucks");
-        item.put(ProductEntry.COLUMN_SUPPLIER_EMAIL, "starbucks@starbucks.com");
-        item.put(ProductEntry.COLUMN_IMAGE_URL, "http://i.imgur.com/YWInHSO.jpg");
-        presetDataList.add(item);
+        presetDataList.add(createDataHashMap(
+                  "Espresso"
+                , "3.00"
+                , "3"
+                , "Starbucks"
+                , "starbucks@starbucks.com"
+                , "http://i.imgur.com/x48dEvw.jpg"
+        ));
 
-        item = new HashMap<>();
-        item.put(ProductEntry.COLUMN_PRODUCT, "Americano");
-        item.put(ProductEntry.COLUMN_PRICE, "3.50");
-        item.put(ProductEntry.COLUMN_QTY, "3");
-        item.put(ProductEntry.COLUMN_SUPPLIER_NAME, "Tim Hortons");
-        item.put(ProductEntry.COLUMN_SUPPLIER_EMAIL, "canada@canada.ca");
-        item.put(ProductEntry.COLUMN_IMAGE_URL, "http://www.themoscowtimes.com/upload/iblock/12e/1280px-A_small_cup_of_coffee.JPG");
-        presetDataList.add(item);
+        presetDataList.add(createDataHashMap(
+                  "Latte"
+                , "4.00"
+                , "3"
+                , "Starbucks"
+                , "starbucks@starbucks.com"
+                , "http://i.imgur.com/YWInHSO.jpg"
+        ));
+
+        presetDataList.add(createDataHashMap(
+                  "Americano"
+                , "3.50"
+                , "3"
+                , "Tim Hortons"
+                , "canada@canada.ca"
+                , "http://www.themoscowtimes.com/upload/iblock/12e/1280px-A_small_cup_of_coffee.JPG"
+        ));
 
         presetData.setSampleData(presetDataList);
         return presetData;
+    }
+
+    private static HashMap<String, String> createDataHashMap(
+              final String product
+            , final String price
+            , final String qty
+            , final String supplierName
+            , final String supplierEmail
+            , final String imageUrl
+    ) {
+        return new HashMap<String, String>() {{
+            put(COLUMN_PRODUCT,product);
+            put(COLUMN_PRICE,price);
+            put(COLUMN_QTY,qty);
+            put(COLUMN_SUPPLIER_NAME,supplierName);
+            put(COLUMN_SUPPLIER_EMAIL,supplierEmail);
+            put(COLUMN_IMAGE_URL,imageUrl);
+        }};
     }
 }
